8528d56640a5b91942aeef8b28825cf298724026
tabarino <tabarino@outlook.com>
2020-06-02 22:36:01 +0100

    Primitive Types and Type Inference In Action - string, number and boolean

---
+++ samples/learning-types.ts
@@ -0,0 +1,8 @@
+// Typescript will implicitly add the types to the variables
+const message = 'Hello World'; // string
+const counter = 1.2; // number
+const messageRead = false; // boolean
+
+console.log(message);
+console.log(counter);
+console.log(messageRead);
