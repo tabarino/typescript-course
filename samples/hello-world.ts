9bece537ed2f4061d1c4db89e259afcf8be4e14d
tabarino <tabarino@outlook.com>
2020-06-02 17:56:39 +0100

    ES6 String Interpolation

--- samples/hello-world.ts
+++ samples/hello-world.ts
@@ -4,11 +4,11 @@ sayHello(message);
 
 function Person(name) {
     this.name = name;
-    this.sayHi = function() {
+    this.sayHi = function () {
         // You can keep using the "this" when you use the arrow function
         // without adding the var self = this;
         setTimeout(() => {
-            console.log('Hello my name is', this.name);
+            console.log(`Hello my name is ${ this.name }`);
         }, 1000)
     }
 }
