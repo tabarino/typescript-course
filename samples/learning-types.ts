d6391147d73c55052ae50407e7de8bedd46632bf
tabarino <tabarino@outlook.com>
2020-06-02 23:39:49 +0100

    Typescript Enums

--- samples/learning-types.ts
+++ samples/learning-types.ts
@@ -1,13 +1,15 @@
-type ThreeName = [string, string, string];
+enum PlayerPosition {
+    Guard,
+    Forward,
+    Center
+}
 
-let persons: ThreeName = ['Michael', 'Shaq', 'Lebron'];
+type Player = [string, PlayerPosition];
 
-let counters: number[] = [0, 1, 2];
+let kobe: Player = ['Kobe', PlayerPosition.Guard];
+let james: Player = ['James', PlayerPosition['Forward']];
+let shaq: Player = ['Shaq', PlayerPosition.Center];
 
-type PlayerTuple = [string, number];
+let players: Player[] = [kobe, james, shaq];
 
-let tuple: PlayerTuple = ['Michael', 1];
-
-console.log(persons);
-console.log(counters);
-console.log(tuple);
+console.log(players);
