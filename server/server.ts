235c361fb11524d3d6a6d699be506db6832d1d6d
tabarino <tabarino@outlook.com>
2020-06-06 20:27:41 +0100

    Setting Up Node Hot Reloading with Nodemon

--- server/server.ts
+++ server/server.ts
@@ -6,10 +6,10 @@ const app: Application = express();
 app.route('/api/courses').get((req, res) => {
     findAllCourses()
         .then(results => {
-            res.status(200).json(results);
+            res.status(200).json({ results });
         });
 });
 
 app.listen(8090, () => {
-    console.log('Server is running on port 8090.');
+    console.log('Server is running on port 8090');
 });
