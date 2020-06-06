19974469a28a97bf3c60fea6626d018418cba8ae
tabarino <tabarino@outlook.com>
2020-06-06 19:54:14 +0100

    Installing Express and its Type Definitions, Setup a Server

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,13 @@
-import { findAllCourses } from './queries/findAllCourses';
+// import { findAllCourses } from './queries/findAllCourses';
+import express from 'express';
 
-findAllCourses()
-    .then(
-        results => console.log(JSON.stringify(results))
-    );
+const app = express();
+
+app.listen(8090, () => {
+    console.log('Server is running on port 8090.');
+});
+
+// findAllCourses()
+//     .then(
+//         results => console.log(JSON.stringify(results))
+//     );
