d18723145e7b6ea0c79871dda15d5535a895a6ab
tabarino <tabarino@outlook.com>
2020-06-06 20:03:48 +0100

    Writing Out First HTTP GET Express Route - Return a List of Courses

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,15 @@
-// import { findAllCourses } from './queries/findAllCourses';
-import express from 'express';
+import { findAllCourses } from './queries/findAllCourses';
+import express, { Application } from 'express';
 
-const app = express();
+const app: Application = express();
+
+app.route('/api/courses').get((req, res) => {
+    findAllCourses()
+        .then(results => {
+            res.status(200).json(results);
+        });
+});
 
 app.listen(8090, () => {
     console.log('Server is running on port 8090.');
 });
-
-// findAllCourses()
-//     .then(
-//         results => console.log(JSON.stringify(results))
-//     );
