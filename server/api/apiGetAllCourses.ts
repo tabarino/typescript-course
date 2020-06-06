acf54d17b9bfe419b9884dd325204595b7f5dfe8
tabarino <tabarino@outlook.com>
2020-06-06 23:37:33 +0100

    How to Prevent Hanging HTTP Requests in Express

--- server/api/apiGetAllCourses.ts
+++ server/api/apiGetAllCourses.ts
@@ -2,10 +2,10 @@ import { findAllCourses } from '../queries/findAllCourses';
 import { Request, Response } from 'express';
 
 export function apiGetAllCourses(req: Request, res: Response) {
-    throw new Error('Error Occurred');
+    // throw new Error('Error Occurred');
 
-    // findAllCourses()
-    //     .then(results => {
-    //         res.status(200).json({ results });
-    //     });
+    findAllCourses()
+        .then(() => { throw new Error('Error Occurred') })
+        .then(results => res.status(200).json({ results }))
+        .catch(err => res.status(500).send());
 }
