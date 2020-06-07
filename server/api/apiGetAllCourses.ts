df9df857901a5a046542cb47484fd3a291a4f956
tabarino <tabarino@outlook.com>
2020-06-07 11:05:42 +0100

    Creating a Reusable Success Handler

--- server/api/apiGetAllCourses.ts
+++ server/api/apiGetAllCourses.ts
@@ -2,12 +2,13 @@ import { findAllCourses } from '../queries/findAllCourses';
 import { Request, Response } from 'express';
 import * as _ from 'lodash';
 import { onError } from './onError';
+import { onSuccess } from './onSuccess';
 
 export function apiGetAllCourses(req: Request, res: Response) {
     // throw new Error('Error Occurred');
 
     findAllCourses()
-        .then(() => { throw new Error('Error Occurred') })
-        .then(results => res.status(200).json({ results }))
+        // .then(() => { throw new Error('Error Occurred') })
+        .then(_.partial(onSuccess, res))
         .catch(_.partial(onError, 'Find All Courses Failed', res));
 }
