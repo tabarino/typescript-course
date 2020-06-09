2c3ea9a5223126a1adc1b29783568ef35a22d18b
tabarino <tabarino@outlook.com>
2020-06-09 12:21:57 +0100

    Class Getters and Setters

--- samples/hello-world.ts
+++ samples/hello-world.ts
@@ -4,19 +4,19 @@ const message = 'Hello World';
 const sayHello = (message: string) => console.log(message);
 sayHello(message);
 
-function Person(name: string) {
-    this.name = name;
-    this.sayHi = function () {
-        // You can keep using the "this" when you use the arrow function
-        // without adding the var self = this;
-        setTimeout(() => {
-            console.log(`Hello my name is ${ this.name }`);
-        }, 1000)
-    }
-}
+// function Person(name: string) {
+    // this.name = name;
+    // this.sayHi = function () {
+    //     // You can keep using the "this" when you use the arrow function
+    //     // without adding the var self = this;
+    //     setTimeout(() => {
+    //         console.log(`Hello my name is ${ this.name }`);
+    //     }, 1000)
+    // }
+// }
 
-const person = new Person('Bob');
-person.sayHi();
+// const person = new Person('Bob');
+// person.sayHi();
 
 const partialAddress = ['Street Name', 'Number'];
 
--- samples/object-oriented.ts
+++ samples/object-oriented.ts
@@ -10,7 +10,8 @@ abstract class SuperHero {
         // Readonly is accessible outside this class and cannot be changed
         readonly editor: Editor,
         // Private is not accessible outside this class
-        private creationYear: number
+        private creationYear: number,
+        public _editor2: Editor
     ) {
     }
 
@@ -50,20 +51,40 @@ class FlyingHero extends SuperHero {
         `;
     }
 
+    get message() {
+        return `
+            Superhero - Using the get keyword:
+            ${this.name}
+            ${Editor[this.editor]}
+        `;
+    }
+
+    get editor2(): string {
+        return Editor[this._editor2];
+    }
+
+    set editor2(editorName: string) {
+        this._editor2 = (<any>Editor)[editorName];
+    }
+
     fly(message: string) {
         console.log(message);
 
-        const superman = new FlyingHero('Superman', Editor.DC, 1938);
+        const superman = new FlyingHero('Superman', Editor.DC, 1938, Editor.DC);
         console.log(superman.createMessage());
         console.log(superman.createMessage2());
     }
 }
 
-const superman = new FlyingHero('Superman', Editor.DC, 1938);
+const superman = new FlyingHero('Superman', Editor.DC, 1938, Editor.DC);
 console.log('Superman: ', superman);
 // console.log(superman.createMessage());
 superman.fly('Up and Away!!!');
 
-const greenLantern = new FlyingHero('Green Lantern', Editor.DC, 1959);
+const greenLantern = new FlyingHero('Green Lantern', Editor.DC, 1959, Editor.DC);
 console.log('Green Lantern: ', greenLantern);
 // console.log(greenLantern.createMessage());
+console.log(greenLantern.message);
+console.log('Editor: ', greenLantern.editor2);
+greenLantern.editor2 = 'MARVEL';
+console.log('Editor: ', greenLantern.editor2);
