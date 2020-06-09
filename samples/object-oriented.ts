91c0854793dfcdad0cbc892e494e55fce6443325
tabarino <tabarino@outlook.com>
2020-06-09 11:29:20 +0100

    Private vs Protected and Method Overrides

--- samples/object-oriented.ts
+++ samples/object-oriented.ts
@@ -15,8 +15,10 @@ class SuperHero {
     }
 
     // The default Access Modifiers of a function is public
-    // private createMessage() {
-    createMessage() {
+    // You can use private createMessage() and it will be available only inside of this class
+    // If we use protected, all classes that inherits this one will be available to see the function
+    // However, if you try to access from somewhere else, it won't be available
+    protected createMessage() {
         return `
             Superhero:
             ${this.name}
@@ -28,16 +30,28 @@ class SuperHero {
 
 // FlyingHero inherits SuperHero
 class FlyingHero extends SuperHero {
+    // We can also override the parent method
+    protected createMessage() {
+        return `
+            Superhero Overridden:
+            ${this.name}
+            ${Editor[this.editor]}
+        `;
+    }
+
     fly(message: string) {
         console.log(message);
+
+        const superman = new FlyingHero('Superman', Editor.DC, 1938);
+        console.log(superman.createMessage());
     }
 }
 
 const superman = new FlyingHero('Superman', Editor.DC, 1938);
 console.log('Superman: ', superman);
-console.log(superman.createMessage());
+// console.log(superman.createMessage());
 superman.fly('Up and Away!!!');
 
 const greenLantern = new FlyingHero('Green Lantern', Editor.DC, 1959);
 console.log('Green Lantern: ', greenLantern);
-console.log(greenLantern.createMessage());
+// console.log(greenLantern.createMessage());
