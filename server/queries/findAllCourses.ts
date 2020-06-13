7485260a3ea0187b31b026733618fd793a9e6633
tabarino <tabarino@outlook.com>
2020-06-13 15:31:19 +0100

    Using Custom Types In Our Application

--- server/queries/findAllCourses.ts
+++ server/queries/findAllCourses.ts
@@ -1,6 +1,7 @@
 import { CourseModel } from '../model/model';
+import { CourseSummary } from '../../shared/model/course-summary';
 
-export function findAllCourses() {
+export function findAllCourses(): Promise<CourseSummary[]> {
     return CourseModel.findAll({
         order: ['seqNo']
     });
