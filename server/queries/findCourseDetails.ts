d23945f00cf9f369a10363f9aec6f9a8bc9f6181
tabarino <tabarino@outlook.com>
2020-06-13 17:56:14 +0100

    Adding a Mapping Function For The Course Details

--- server/queries/findCourseDetails.ts
+++ server/queries/findCourseDetails.ts
@@ -1,9 +1,11 @@
 import { CourseModel, LessonModel } from '../model/model';
+import { CourseDetails, createCourseDetails } from '../../shared/model/course-details';
+import Bluebird from 'bluebird';
 
-export function findCourseDetails(courseId: number) {
+export function findCourseDetails(courseId: number): Bluebird<CourseDetails> {
     return CourseModel.findByPk(courseId, {
         include: [{
             model: LessonModel
         }]
-    });
+    }).then(createCourseDetails);
 }
--- shared/model/course-details.ts
+++ shared/model/course-details.ts
@@ -6,5 +6,29 @@ export interface CourseDetails extends CourseSummary {
     comingSoon?: boolean;
     isNew: boolean;
     isOngoing?: boolean;
-    lessons: Lesson[];
+    Lessons: Lesson[];
+}
+
+export function createCourseDetails({
+    id, description, url, iconUrl, courseListIcon, seqNo, longDescription, comingSoon, isNew, isOngoing, Lessons
+}: any): CourseDetails {
+    return {
+        id, description, url, iconUrl, courseListIcon, seqNo, longDescription, comingSoon, isNew, isOngoing,
+        Lessons: Lessons.map(createLessonFromDbModel)
+    };
+}
+
+function createLessonFromDbModel({
+    id, url, description, duration, seqNo, courseId, pro, tags
+}: any) {
+    return {
+        id,
+        url,
+        description,
+        duration,
+        seqNo,
+        courseId,
+        pro,
+        tags
+    };
 }
