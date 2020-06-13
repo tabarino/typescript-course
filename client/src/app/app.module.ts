f794112245d523ed927bcace83dcdf442cc0fe34
tabarino <tabarino@outlook.com>
2020-06-13 18:32:09 +0100

    Create the Course Detail Component Using The Angular CLI

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CourseDetailsComponent } from './course-details/course-details.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CourseDetailsComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ client/src/app/course-details/course-details.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'course-details',
+    templateUrl: './course-details.component.html',
+    styleUrls: ['./course-details.component.css']
+})
+export class CourseDetailsComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
