bd7a071cf811663dd11054de9ce63d82b85b727f
tabarino <tabarino@outlook.com>
2020-06-13 15:17:35 +0100

    Improving the Model of Our Application

---
+++ shared/model/course-summary.ts
@@ -0,0 +1,7 @@
+export interface CourseSummary {
+    description: string;
+    readonly url: string;
+    readonly iconUrl: string;
+    readonly courseListIcon: string;
+    readonly seqNo: number;
+}
