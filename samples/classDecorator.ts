ebbe66cd4b765483ef102ff4c3e763d55ce3a672
tabarino <tabarino@outlook.com>
2020-06-13 14:40:58 +0100

    Property Decorators

--- samples/classDecorator.ts
+++ samples/classDecorator.ts
@@ -4,12 +4,22 @@ function Entity(tableName: string) {
     }
 }
 
+function Column(columnName: string) {
+    return (target: any, propertyKey: string) => {
+        console.log(propertyKey);
+    }
+}
+
 function registerOrmModel(model: Function) {
     console.log('Registering ORM Models', model);
 }
 
 @Entity('todos')
 class Todo {
+    @Column('name')
+    name: string;
+
     constructor(public description: string, public done = false) {
+        this.name = 'Ivan';
     }
 }
